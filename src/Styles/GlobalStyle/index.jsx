c2cfcf9d37abfd35071ec84ef62683e728c2e82f
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-08-05 01:09:40 -0300

    fix: test two

--- src/Styles/GlobalStyle/index.jsx
+++ src/Styles/GlobalStyle/index.jsx
@@ -1,17 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-body {
-  background-image: url("https://assets.entrepreneur.com/content/3x2/2000/20160628101609-Coding.jpeg");
-  margin: 0;
-  padding: 0;
-  font-family: Open-Sans, Helvetica, Sans-Serif;
-  background-position: center;
-  background-size: 117vw;
-  
-    @media (max-width: 500px) {
-        background-size: 100vh;
-    }
-}
+
+  body {
+    background-image: url("https://assets.entrepreneur.com/content/3x2/2000/20160628101609-Coding.jpeg");
+    margin: 0;
+    padding: 0;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    background-position: center;
+    background-size: 117vw;
+
+      @media (max-width: 500px) {
+          background-size: 100vh;
+      }
+  }
 `
 export default GlobalStyle
